541aa06a441e24e94f83a2f223d08e6645afa014
liuchenxi12 <121725453+liuchenxi12@users.noreply.github.com>
2022-12-31 23:54:38 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+var design = anime({
+  targets: '#newyear2023 #happy',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 2000,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
+var design = anime({
+  targets: '#newyear2023 #NEWYEAR',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 2500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
+
+
+var design = anime({
+  targets: '#newyear2023 #Vector_43,#Vector_210,#Vector_207,#Vector_42,#Vector_45',
+  translateY: -10,
+  easing: 'easeInOutSine',
+  duration: 2500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+  loop: true
+});
+
